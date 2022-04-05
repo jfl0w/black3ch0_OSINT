2673de196d43eb655f9292eb15be728ab724b30a
jfl0w <jimmy.pham25@gmail.com>
2022-04-04 19:18:36 -0700

    Update cru.sh

--- cru.sh
+++ cru.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
-# Using curl to measure HTTP response times against invalid and valid requests. Testing for HTTP Proxy
-#
-#"First Byte: "%{time_pretransfer}\\n
-#"DNS Lookup:  "%{time_namelookup}\\n
+# Bash script to test for HTTP proxy against HTTP response times for invalid and valid requests.
+# Curl time options: 
+              # time_appconnect
+                     # The time, in seconds, it took from the start until
+                     # the SSL/SSH/etc connect/handshake to the remote
+                     # host was completed. 
+
+              # time_connect
+                     # The time, in seconds, it took from the start until
+                     # the TCP connect to the remote host (or proxy) was
+                     # completed.
+
+              # time_namelookup
+                     # The time, in seconds, it took from the start until
+                     # the name resolving was completed.
+
+              # time_pretransfer
+                     # The time, in seconds, it took from the start until
+                     # the file transfer was just about to begin. This
+                     # includes all pre-transfer commands and negotiations
+                     # that are specific to the particular protocol(s)
+                     # involved.
+
+              # time_redirect
+                     # The time, in seconds, it took for all redirection
+                     # steps including name lookup, connect, pretransfer
+                     # and transfer before the final transaction was
+                     # started. time_redirect shows the complete execution
+                     # time for multiple redirections. 
+
+              # time_starttransfer
+                     # The time, in seconds, it took from the start until
+                     # the first byte was just about to be transferred.
+                     # This includes time_pretransfer and also the time
+                     # the server needed to calculate the result.
+
+              # time_total
+                     # The total time, in seconds, that the full operation
+                     # lasted.
+
+              # url  - The URL that was fetched. 
+
 
 blank_line= echo " "
 
